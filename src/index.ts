0a5fc1f018846100e191d3ff759f0980c6aae917
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-28 08:57:45 +0300

    Update success message for package index.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import CreateCLI from './CreateCLI';
 import Chalk from 'chalk';
 
-CreateCLI().catch((error) => {
+CreateCLI().then((products) => {
+    console.log('\n');
+    console.log(Chalk.greenBright(`Successfully downloaded all ${Chalk.yellowBright(products.length.toLocaleString())} products from Selly.io!`))
+    console.log(Chalk.greenBright('Check the current directory for your products. \n'));
+}).catch((error) => {
     console.error(Chalk.bgRed(error.message));
     process.exit(1);
 });
