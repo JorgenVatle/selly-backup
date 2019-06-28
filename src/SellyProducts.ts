06ca46e401d1d237167ab701f39ea98b20179f56
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-28 07:05:33 +0300

    Add all() method to SellyProduct.

--- src/SellyProducts.ts
+++ src/SellyProducts.ts
@@ -8,6 +8,11 @@ interface SellyProductsOptions {
     apiKey: string;
 }
 
+/**
+ * Selly product.
+ */
+type Product = any;
+
 export default class SellyProducts {
 
     /**
@@ -31,8 +36,22 @@ export default class SellyProducts {
     /**
      * Get products from the given page for the current merchant.
      */
-    protected getPage(page: number = 1) {
+    protected getPage(page: number = 1): Promise<Product[]> {
         return this.API.get('/products', { params: { page } }).then((response) => response.data);
+    }
+
+    /**
+     * Fetch all products for the current shop.
+     */
+    public async all() {
+        let page: undefined | Array<Product>;
+        let result: Array<Product> = [];
+        let currentPage = 0;
+
+        while(typeof page === 'undefined' || page.length) {
+            page = await this.getPage(currentPage += 1);
+            result = [...result, ...page];
+        }
     }
 
 }
