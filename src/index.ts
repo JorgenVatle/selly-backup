c6749c2d0e53548bdefdc9df673f9dd91c6d060a
Jørgen Vatle <Jorgen@WcAServices.net>
2019-06-28 09:03:31 +0300

    Correct code formatting in src/index.ts.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import Chalk from 'chalk';
 
 CreateCLI().then((products) => {
     console.log('\n');
-    console.log(Chalk.greenBright(`Successfully downloaded all ${Chalk.yellowBright(products.length.toLocaleString())} products from Selly.io!`))
+    console.log(Chalk.greenBright(`Successfully downloaded all ${Chalk.yellowBright(products.length.toLocaleString())} products from Selly.io!`));
     console.log(Chalk.greenBright('Check the current directory for your products. \n'));
 }).catch((error) => {
     console.error(Chalk.bgRed(error.message));
